762c45d04ef566797240cda7de50164a63ad958a
Norman Frieman <ezequiel-d.s@hotmail.com>
2024-03-01 00:45:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+https://github.com/healeycodes/doom-checkboxes
+https://github.com/diekmann/wasm-fizzbuzz
---
+++ glue.js
@@ -0,0 +1,31 @@
+const canvas = document.querySelector("canvas");
+const video = document.querySelector("#doom-video");
+
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1572422
+// Looks like canvas.captureStream() doesn't work unless
+//    you've already called canvas.getContext()
+canvas.getContext("2d");
+setTimeout(() => (video.srcObject = canvas.captureStream()), 0);
+
+document.body.onmousedown = () => {
+    if (window.doomLoaded !== true) {
+        return;
+    }
+    video.play();
+};
+
+const forwardKey = (e, type) => {
+    const ev = new KeyboardEvent(type, {
+        key: e.key,
+        keyCode: e.keyCode,
+    });
+    canvas.dispatchEvent(ev);
+};
+
+document.body.addEventListener("keydown", function(e) {
+    forwardKey(e, "keydown");
+});
+
+document.body.addEventListener("keyup", function(e) {
+    forwardKey(e, "keyup");
+});
